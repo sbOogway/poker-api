b5968d1bad129f884f933f6e0d0c5b85670fd9c5
Rodrigo Agundez <rragundez@gmail.com>
2025-10-21 21:58:55 +0800

    Add health check endpoints

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from .health import router as health_router
 from .login import router as login_router
 from .logout import router as logout_router
 from .posts import router as posts_router
@@ -9,6 +10,7 @@ from .tiers import router as tiers_router
 from .users import router as users_router
 
 router = APIRouter(prefix="/v1")
+router.include_router(health_router)
 router.include_router(login_router)
 router.include_router(logout_router)
 router.include_router(users_router)
