a35be82d17b03b9ec4f1a1c41a1b1ef4535c3a28
Rodrigo Agundez <rragundez@gmail.com>
2025-11-19 01:10:08 +0700

    Add default value for secret key

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -21,7 +21,7 @@ class AppSettings(BaseSettings):
 
 
 class CryptSettings(BaseSettings):
-    SECRET_KEY: SecretStr
+    SECRET_KEY: SecretStr = SecretStr("secret-key")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
